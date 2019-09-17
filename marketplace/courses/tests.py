69983b6715d5b5b00e6c3e1a12ddae1b7b491240
aidarbek <suleimenov.aidarbek@gmail.com>
2019-09-17 10:05:11 +0100

    Django skeleton created

---
+++ marketplace/courses/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+import datetime
+
+class BasicTests(TestCase):
+
+    def test_index_page_returns_200_code(self):
+        """
+        returns true, if starting page always returns something
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
