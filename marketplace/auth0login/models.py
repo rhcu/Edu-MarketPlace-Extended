bc09d99dfa372240f324af74620f3578af9bd152
aidarbek <suleimenov.aidarbek@gmail.com>
2019-09-21 16:25:16 +0100

    Model deleted, since the same already exists

--- marketplace/auth0login/models.py
+++ marketplace/auth0login/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class UserData(models.Model):
-    username = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=200)
-    picture = models.CharField(max_length=200)
-    user_id = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    date = models.DateTimeField('date published')
