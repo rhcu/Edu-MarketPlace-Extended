e0f15974a4e4d7e0dc6fa44fc14c17dc73561c97
aishaadambek <aishabibi.adambek@nu.edu.kz>
2019-10-01 18:30:47 +0600

    Make some User and Profile info updatable

--- marketplace/auth0login/models.py
+++ marketplace/auth0login/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -10,6 +11,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 @receiver(post_save, sender=User)
