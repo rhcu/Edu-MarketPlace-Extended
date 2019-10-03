1506dcf5d5f3127417e8338f8c6f7d05ecbb2ffb
Aishabibi Adambek <aishabibi.adambek@nu.edu.kz>
2019-10-04 02:30:53 +0600

    Make some User and Profile info updatable (#30)
    * Fix #28: add a Profile model with a one-to-one correspondence to User model
    
    * Make some User and Profile info updatable
    
    * Make some User and Profile info updatable

--- marketplace/auth0login/views.py
+++ marketplace/auth0login/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as log_out
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from urllib.parse import urlencode
 from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileUpdateForm
 
 
 def index(request):
@@ -40,4 +42,23 @@ def logout(request):
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect(profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, 'profile.html', context)
