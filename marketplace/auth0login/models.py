0d26fc27444184e05f4b24cd79f3756343828878
aishaadambek <aishabibi.adambek@nu.edu.kz>
2019-10-08 12:12:35 +0600

    Fix #27: Configure remote storage system

--- marketplace/auth0login/models.py
+++ marketplace/auth0login/models.py
@@ -12,16 +12,6 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, **kwargs):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
