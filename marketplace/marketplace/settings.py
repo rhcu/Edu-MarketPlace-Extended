03beba32330ed02d09c14121d3ee0681b8e23b74
Aishabibi Adambek <aishabibi.adambek@nu.edu.kz>
2019-10-08 13:53:15 +0600

    Fix #27: Configure remote storage system (#35)

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'courses',
     'core',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -166,3 +167,12 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
