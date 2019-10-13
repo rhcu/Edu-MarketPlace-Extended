6f997d043d137c65f5ca279db72df0f0765e5fe9
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2019-10-13 13:11:30 +0100

    Add price field to course and button to enroll

--- marketplace/courses/models.py
+++ marketplace/courses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from decimal import Decimal
 
 class Course(models.Model):
     title = models.CharField(max_length=500)
@@ -8,6 +8,7 @@ class Course(models.Model):
     topic = models.CharField(max_length=200)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     date_created = models.DateTimeField('date published')
+    price = models.DecimalField(max_digits=9, decimal_places=2, default=Decimal('0.0000'))
 
     def __str__(self):
         return self.title
