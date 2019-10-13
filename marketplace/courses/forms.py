ac083a143a61af044699a9e08cba30439a3f5773
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2019-10-13 13:22:41 +0100

    Add price field to course and button to enroll (#43)
    * Add price field to course and button to enroll
    
    * Remove unused migrations

--- marketplace/courses/forms.py
+++ marketplace/courses/forms.py
@@ -6,4 +6,4 @@ class CourseForm(forms.ModelForm):
 
     class Meta:
         model = Course
-        fields = ('title', 'description', 'topic',)
+        fields = ('title', 'description', 'topic', 'price')
