fdc98448609fdb13524b923b586fe7600152ff15
Aishabibi Adambek <aishabibi.adambek@nu.edu.kz>
2019-10-16 16:23:20 +0600

    Make static files visible to Heroku [2] (#46)

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'marketplace.urls'
