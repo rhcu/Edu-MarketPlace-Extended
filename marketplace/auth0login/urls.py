901db7a5a40a794ca6ea19ec0eaca97ba4ec15b1
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2019-10-19 15:49:47 +0100

    Added FAQ

--- marketplace/auth0login/urls.py
+++ marketplace/auth0login/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout', views.logout),
     path('profile', views.profile),
     path('contact_us', views.contact_us),
+    path('faq', views.faq),
     path('', include('django.contrib.auth.urls')),
     path('', include('social_django.urls')),
 ]
