0cca8ac6b405c014a6c888a4eb62748ea96b5ddc
Aidarbek Suleimenov <suleimenov.aidarbek@gmail.com>
2019-11-11 10:09:11 +0000

    Quiz creation added (although users can't submit them yet) (#74)
    * Quiz creation added (although users can't submit them yet)
    
    * migrations added
    
    * Test fix
    
    * Update save_video.html

--- marketplace/courses/models.py
+++ marketplace/courses/models.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 
 
 class Course(models.Model):
+    # Information about the course
     title = models.CharField(max_length=500)
     description = models.CharField(max_length=2000)
     topic = models.CharField(max_length=200)
@@ -19,10 +20,13 @@ class Course(models.Model):
 ENTRIES_CHOICES = (
     ('lesson', 'Lesson'),
     ('video', 'Video'),
+    ('quiz', 'Quiz'),
 )
 
 
 class CourseEntry(models.Model):
+    # Meta data about course entry, full data stored within course
+    # entry types
     name = models.CharField(max_length=500)
     entry_type = models.CharField(max_length=100, choices=ENTRIES_CHOICES, default='lesson')
     course = models.ForeignKey(Course, on_delete=models.CASCADE,)
@@ -30,18 +34,39 @@ class CourseEntry(models.Model):
 
 
 class Lesson(models.Model):
+    # Lesson (rich text) type of course entry
     course_entry = models.ForeignKey(CourseEntry, on_delete=models.CASCADE,)
     content = models.TextField()
 
 
 class Video(models.Model):
+    # Video type of course entry
     course_entry = models.ForeignKey(CourseEntry, on_delete=models.CASCADE,)
     video_url = models.URLField(max_length=1000)
 
 
 class CourseEnroll(models.Model):
+    # User enrollment storage
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.course.title + " id=" + str(self.course_id) + ", enrolled: " + self.user.username
+
+
+class Quiz(models.Model):
+    # Quiz type of course entry
+    course_entry = models.ForeignKey(CourseEntry, on_delete=models.CASCADE,)
+
+
+class Question(models.Model):
+    # Many questions per one quiz
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE,)
+    name = models.CharField(max_length=200)
+
+
+class Answer(models.Model):
+    # Many possible answers per one question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,)
+    name = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
