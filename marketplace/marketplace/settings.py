00d38c9d2be16418071bcf7b235bd2f75f71d942
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2020-02-11 01:59:12 +0600

    Localize to Russian (#99)
    * Initial localization step
    
    * Some more steps
    
    * Language changed to Russian
    
    * More fixes
    
    * Translations
    
    * Fixed untranslated parts
    
    * Removed unnecessary changes
    
    Co-authored-by: Aidarbek Suleimenov <suleimenov.aidarbek@gmail.com>

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,8 +126,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Russian'),
+]
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
