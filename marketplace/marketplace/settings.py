8e4cb5a856ddd1de2055ffe394693a50be0aa1f2
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2020-02-10 17:40:47 +0600

    Initial localization step

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fi'
 
 TIME_ZONE = 'UTC'
 
@@ -126,7 +126,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOCALE_PATHS = (
+	os.path.join(BASE_DIR, 'locale'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
